aa30dfa1fb01d757fcdeabec952592b56d398793
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-09-07 14:19:34 -0700

    added game logic

--- battleship.js
+++ battleship.js
@@ -20,6 +20,7 @@ var letterConversion = {
 	"I": 8,
 	"J": 9
 }
+ 	var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
@@ -27,11 +28,16 @@ for (i = 0; i < cols; i++) {
 
 		// creates a new div HTML element for each grid square and makes it the right size
 		var square = document.createElement("div");
+
 		gameBoardContainer.appendChild(square);
 
     // give each div element a unique id based on its row and column, like "s00"
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
+
+// THIS IS WHWERE YOU WILL ADDD CRE FOR PART 1 TO ADD TEXXT TO EACH SQUARE
+		square.textContent = letterArray[j] + (i + 1);
+
 
 
 		// set each grid square's coordinates: multiples of the current row or column number
@@ -60,6 +66,17 @@ var gameBoard = [
 
 function fireTorpedo() {
 
-	// Your game logic will go here!
+
+	var row = letterConversion[nowLetter];
+	var userInput = $("#textInput").val();
+	var nowLetter = userInput.substring(0,1);
+	var column = userInput.substring(1.2);
+
+	userInput = document.getElementById("exampleInput").value;
+	rowLetter = userInput.substring(0,1);
+	console.log(rowLetter);
+
+
+
 
 }
